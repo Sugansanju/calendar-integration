302348222cf5a0825d170be0e2142e55485f0726
sugansanju <suganjo15@gmail.com>
2024-06-23 16:01:54 +0530

    CORS added

--- middleware.js
+++ middleware.js
@@ -1,22 +1,44 @@
-import { NextResponse } from "next/server";
-import { jwtDecode } from "jwt-decode";
-import moment from "moment";
-import { parseDateString } from "@/utils";
+import { NextResponse } from 'next/server';
+import jwtDecode from 'jwt-decode';
+import moment from 'moment';
+import { parseDateString } from '@/utils';
+import Cors from 'cors';
 
-const Middleware = (req) => {
-  let NEXT_TOKEN ="ta_access_token";
-  console.log("====",NEXT_TOKEN)
+// Initialize the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  origin: '*', // Allow all origins (you can restrict this to specific origins)
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+const Middleware = async (req) => {
+  // Run the CORS middleware
+  await runMiddleware(req, res, cors);
+
+  let NEXT_TOKEN = "ta_access_token";
+  console.log("====", NEXT_TOKEN);
   const cookieData = req.cookies.get(NEXT_TOKEN);
   const pathName = req.nextUrl.pathname;
-  console.log("--Pathname---",pathName)
+  console.log("--Pathname---", pathName);
   if (pathName === '/favicon.ico') {
     return NextResponse.next();
   }
 
   if (pathName.toLowerCase() === "/login") {
-    console.log("==If===")
+    console.log("==If===");
     if (cookieData?.value) {
-      // return NextResponse.redirect("http://localhost:3000");
       return NextResponse.redirect("https://calendar-integration-001.vercel.app");
     }
   }
@@ -33,14 +55,12 @@ const Middleware = (req) => {
       let parsedEndTime = parseDateString(endTime);
       if (decoded && parsedBeginningTime > parsedEndTime) {
         req.cookies.has(NEXT_TOKEN) && req.cookies.delete(NEXT_TOKEN);
-        // return NextResponse.redirect("http://localhost:3000" + "/login");
         return NextResponse.redirect("https://calendar-integration-001.vercel.app" + "/login");
       } else {
         return NextResponse.next();
       }
     } else {
       return NextResponse.redirect("https://calendar-integration-001.vercel.app" + "/login");
-      // return NextResponse.redirect("http://localhost:3000" + "/login");
     }
   }
   return NextResponse.next();
